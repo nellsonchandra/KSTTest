d3e99be858fa3965624089f6d617e264c18332a0
nellsonchandra <32291440+nellsonchandra@users.noreply.github.com>
2021-10-22 15:42:11 +0700

    Add files via upload

---
+++ KSTTest/KSTTest/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace KSTTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSort = new System.Windows.Forms.Button();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.lblText1 = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(12, 82);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(99, 23);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort !";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // rtb1
+            // 
+            this.rtb1.Location = new System.Drawing.Point(12, 120);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(776, 318);
+            this.rtb1.TabIndex = 2;
+            this.rtb1.Text = "";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(12, 35);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(99, 23);
+            this.btnSelect.TabIndex = 3;
+            this.btnSelect.Text = "Select File";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // lblText1
+            // 
+            this.lblText1.AutoSize = true;
+            this.lblText1.Location = new System.Drawing.Point(12, 19);
+            this.lblText1.Name = "lblText1";
+            this.lblText1.Size = new System.Drawing.Size(71, 13);
+            this.lblText1.TabIndex = 4;
+            this.lblText1.Text = "SelectedFile :";
+            this.lblText1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(89, 19);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(0, 13);
+            this.lblFilePath.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.lblText1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.btnSort);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Label lblText1;
+        private System.Windows.Forms.Label lblFilePath;
+    }
+}
+
---
+++ KSTTest/KSTTest/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Collections;
+
+namespace KSTTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        class nameSorting
+        {
+            public string lastName { get; set; }
+            public string givenName { get; set; }
+           public string restOfName { get; set; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            string filename="";
+            var loadDialog = new OpenFileDialog { Filter = "Text File|*.txt", InitialDirectory = @"C:\Your\Start\Directory\" };
+            if (loadDialog.ShowDialog() == DialogResult.OK)
+            {
+                filename = loadDialog.FileName;
+                lblFilePath.Text = filename.ToString();
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            string[] lines = System.IO.File.ReadAllLines
+            (@""+lblFilePath.Text);
+
+            System.Diagnostics.Trace.WriteLine("Contents of unsorted-names-list.txt =:");
+            nameSorting[] allNames = new nameSorting[10000];
+            ArrayList arrList = new ArrayList();
+
+            int count = 0;
+            string theRestOfName;
+
+            //get all the names that are in text file and start the loop.
+            foreach (string line in lines)
+            {
+                string[] nameSplit = line.Split(' ');
+                if (nameSplit.Count() > 2)
+                {
+                    if (nameSplit.Count() == 4)
+                    {
+                        theRestOfName = nameSplit[0] + " " + nameSplit[1]; //handle if the name has 3 given name.
+                    }
+                    else
+                    {
+                        theRestOfName = nameSplit[0]; //handle if the name has 2 given name
+                    }
+                    allNames[count] = new nameSorting
+                    {
+                        lastName = nameSplit[nameSplit.Count() - 1],
+                        givenName = nameSplit[nameSplit.Count() - 2],
+                        restOfName = theRestOfName
+                    };
+
+                }
+                else
+                {
+                    //handle if the name only consist of LastName and GivenName.
+                    allNames[count] = new nameSorting
+                    {
+                        lastName = nameSplit[nameSplit.Count() - 1],
+                        givenName = nameSplit[nameSplit.Count() - 2],
+                        restOfName = ""
+                    };
+                }
+                count++;
+            }
+
+            //Sort all the names in array using LINQ, sort by Last name, then the given name.
+            allNames = allNames.Where(x => x != null).ToArray();
+            allNames = allNames.OrderBy(x => x.lastName).ThenBy(x => x.givenName).ToArray();
+
+            //Write all the sorted names in array into a text file in current executable path, before writing the values of array, write an empty string as a way to "Rewrite" everything
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            File.WriteAllText(path + @"\" + "sorted-names-list.txt", string.Empty);
+            foreach (var x in allNames)
+            {
+                if (x.restOfName != "")
+                {
+                    File.AppendAllText(path + @"\" + "sorted-names-list.txt", x.restOfName + " " + x.givenName + " " + x.lastName + "\r\n");
+
+                    rtb1.AppendText(x.restOfName + " " + x.givenName + " " + x.lastName + "\n");
+                    rtb1.ScrollToCaret();
+                }
+                else
+                {
+                    File.AppendAllText(path + @"\" + "sorted-names-list.txt", x.givenName + " " + x.lastName + "\r\n");
+
+                    rtb1.AppendText(x.givenName + " " + x.lastName + "\n");
+                    rtb1.ScrollToCaret();
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            MessageBox.Show("Sorted Successfully !");
+            System.Diagnostics.Trace.WriteLine("Press any key to exit.");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
